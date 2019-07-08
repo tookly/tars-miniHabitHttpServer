4cead039c1452f768adfda95b0f492e4eaf75d7b
chengxiaoli <chengxiaoli@qmtv.com>
2019-07-08 23:22:23 +0800

    fix target api

--- src/controller/TargetController.php
+++ src/controller/TargetController.php
@@ -14,17 +14,17 @@ use HttpServer\component\Controller;
 class TargetController extends Controller
 {
     const TARGET_KEY = 'HASH:TARGET:%s';
-    const TARGET_NOTE_KEY = 'LIST:TARGET:NOTE:%s';
+    const TARGET_NOTE_KEY = 'ZSET:TARGET:NOTE:%s';
     const TARGET_SIGN_KEY = 'ZSET:TARGET:SIGN:%s';
-    const TARGET_SIGN_LOG_KEY = 'LIST:TARGET:SIGN:LOG:%s:%s'; // 最好还是落db，先不加了
+    const TARGET_SIGN_LOG_KEY = 'LIST:TARGET:SIGN:LOG:%s:%s';
     
     /**
      * @throws \exception
      */
     public function actionInfo()
     {
-        $targetId = $this->getGet('targetId');
-        $data = Redis::instance()->hGetAll(sprintf(self::TARGET_KEY, $targetId)) ?: [];
+        $targetId = $this->getGet('targetId', 1);
+        $data = Redis::instance()->hGetAll(sprintf(self::TARGET_KEY, $targetId)) ?: null;
         $this->sendSuccess($data);
     }
     
@@ -33,9 +33,9 @@ class TargetController extends Controller
      */
     public function actionSet()
     {
-        $data['target'] = $this->getPost('target');
-        $data['targetId'] = $this->getPost('targetId');
-        if (empty($data['target']) || $data['targetId'] <= 0) {
+        $data['targetId'] = $this->getPost('targetId', 1);
+        $data['target'] = $this->getPost('target', '');
+        if (empty($data['target']) || empty($data['targetId'])) {
             $this->sendParamErr();
         }
         Redis::instance()->hMSet(sprintf(self::TARGET_KEY, $data['targetId']), $data);
@@ -47,12 +47,12 @@ class TargetController extends Controller
      */
     public function actionNotes()
     {
-        $targetId = $this->getGet('targetId');
-        $lines = Redis::instance()->lRange(sprintf(self::TARGET_NOTE_KEY, $targetId), 0, -1);
+        $targetId = $this->getGet('targetId', 1);
+        $lines = Redis::instance()->zRevRange(sprintf(self::TARGET_NOTE_KEY, $targetId), 0, -1, true);
         $notes = [];
-        foreach ($lines as $line) {
+        foreach ($lines as $dateline => $line) {
             $note = json_decode($line, true);
-            if (empty($note) || empty($note['line'] || empty($note['dateline']))) {
+            if (empty($note)) {
                 continue;
             }
             $notes[] = $note;
@@ -66,13 +66,14 @@ class TargetController extends Controller
      */
     public function actionNote()
     {
-        $targetId = $this->getPost('targetId');
-        $note = $this->getPost('note');
-        if (empty($note) || $targetId <= 0) {
+        $targetId = $this->getPost('targetId', 1);
+        $note = $this->getPost('note', '');
+        if (empty($note) || empty($targetId)) {
             $this->sendParamErr();
         }
-        $line = json_encode(['note' => $note, 'dateline' => time()]);
-        Redis::instance()->lPush(sprintf(self::TARGET_NOTE_KEY, $targetId), $line);
+        $dateline = time();
+        $line = json_encode(['note' => $note, 'dateline' => $dateline]);
+        Redis::instance()->zAdd(sprintf(self::TARGET_NOTE_KEY, $targetId), $dateline, $line);
         $this->sendSuccess();
     }
     
@@ -81,15 +82,14 @@ class TargetController extends Controller
      */
     public function actionSign()
     {
-        $targetId = $this->getPost('targetId');
+        $targetId = $this->getPost('targetId', 1);
         $unit = $this->getPost('unit', 1);
-        if ($targetId <= 0) {
+        if (empty($targetId) || empty($unit)) {
             $this->sendParamErr();
         }
         $date = date('Ymd', time());
         $data['count'] = Redis::instance()->zIncrBy(sprintf(self::TARGET_SIGN_KEY, $targetId), $unit, $date);
         
-        // todo log
         $log['targetId'] = $targetId;
         $log['unit'] = $unit;
         $log['dateline'] = time();
@@ -98,8 +98,25 @@ class TargetController extends Controller
         $this->sendSuccess($data);
     }
     
+    /**
+     * @throws \Exception
+     */
     public function actionStatistics()
     {
-        $targetId = $this->getPost('targetId');
+        $targetId = $this->getGet('targetId', 1);
+        $end = time();
+        $start = $end - 86400 * 20;
+        $endDate = date('Ymd', $end);
+        $startDate = date('Ymd', $start);
+        $records = Redis::instance()->zRevRangeByScore(sprintf(self::TARGET_SIGN_KEY, $targetId), $startDate, $endDate, true);
+        $data = [];
+        for($i = $start; $i <= $end; $i = $i - 86400) {
+            $date = date('Ymd', $i);
+            $tmp['date'] = $date;
+            $tmp['times'] = $records[$date] ?? 0;
+            $data[] = $tmp;
+        }
+        $this->sendSuccess($data);
     }
+    
 }
